a97169b41a32769c0e7d55ffc47bd4ca27d86e3c
marcus <marcus.rodrigues10@hotmail.com>
2021-07-30 18:20:41 -0300

    creating timer logic

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import React, { Component } from 'react';
+import Timer from './Components/Timer';
 
 export default class App extends Component {
   render() {
     return (
-      
+      <div>
+        <Timer />
+      </div>
     );
   }
 }
