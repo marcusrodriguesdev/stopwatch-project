f597fdefaa6402d3005a483a8d1effe1f5d9092f
marcus <marcus.rodrigues10@hotmail.com>
2021-08-03 18:41:50 -0300

    create inputs of time

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
+import React from 'react';
 import './App.css';
-import React, { Component } from 'react';
-import Timer from './Components/Timer';
+import Cronometer from './Components/Cronometer';
 
-export default class App extends Component {
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      startChronometer: false,
+    };
+  }
+
   render() {
     return (
-      <div>
-        <Timer />
-      </div>
+      <main>
+        <h1>Cronômetro</h1>
+        <section>
+          <Cronometer />
+        </section>
+      </main>
     );
   }
 }
